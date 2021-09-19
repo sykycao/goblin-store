70ca841130abe8b7c3e1828b0ee2bde4ae9245d8
Kỳ Cao <sykycao@gmail.com>
2021-09-19 21:34:07 +0700

    add testHelpers function

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import React from "react"
-import { Switch, Route } from "react-router-dom"
-import { Checkout } from "./Checkout"
-import { Home } from "./Home"
-import { Cart } from "./Cart"
-import { Header } from "./shared/Header"
-import { OrderSummary } from "./OrderSummary"
-
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { Checkout } from './Checkout';
+import { Home } from './Home';
+import { Cart } from './Cart';
+import { Header } from './shared/Header';
+import { OrderSummary } from './OrderSummary';
 
 export const App = () => {
   return (
     <>
-      <Header/>
+      <Header />
       <div className="container">
         <Switch>
           <Route path="/checkout">
@@ -22,11 +21,12 @@ export const App = () => {
           <Route path="/order">
             <OrderSummary />
           </Route>
-          <Route path="/">
+          <Route exact path="/">
             <Home />
           </Route>
+          <Route>Page not found!</Route>
         </Switch>
       </div>
     </>
-  )
-}
+  );
+};
