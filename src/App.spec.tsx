b006d1b7c8a41983a657f0b06ae7eb9b1d9c5bc1
Kỳ Cao <sykycao@gmail.com>
2021-09-19 21:58:55 +0700

    use renderWithRouter to test routes

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -8,6 +8,40 @@ jest.mock('./Home', () => ({
   Home: () => <div>Home</div>,
 }));
 
+jest.mock('./Cart', () => ({
+  Cart: () => <div>Cart</div>,
+}));
+
+jest.mock('./Checkout', () => ({
+  Checkout: () => <div>Checkout</div>,
+}));
+
+jest.mock('./OrderSummary', () => ({
+  OrderSummary: () => <div>Order summary</div>,
+}));
+
+describe('routing', () => {
+  it("renders home page on '/'", () => {
+    const { container } = renderWithRouter(() => <App />, '/');
+    expect(container.innerHTML).toMatch('Home');
+  });
+
+  it("renders checkout page on '/cart'", () => {
+    const { container } = renderWithRouter(() => <App />, '/cart');
+    expect(container.innerHTML).toMatch('Cart');
+  });
+
+  it("renders order summary page on '/order'", () => {
+    const { container } = renderWithRouter(() => <App />, '/order');
+    expect(container.innerHTML).toMatch('Order summary');
+  });
+
+  it('renders the page on nonexistent routes', () => {
+    const { container } = renderWithRouter(() => <App />, '/nonexistent_route');
+    expect(container.innerHTML).toMatch('Page not found!');
+  });
+});
+
 describe('App', () => {
   it('renders successfully', () => {
     const history = createMemoryHistory();
@@ -17,16 +51,5 @@ describe('App', () => {
       </Router>
     );
     expect(container.innerHTML).toMatch('Goblin Store');
-  });
-
-  it('renders Home component on root route', () => {
-    const history = createMemoryHistory();
-    history.push('/');
-    const { container } = render(
-      <Router history={history}>
-        <App />
-      </Router>
-    );
-    expect(container.innerHTML).toMatch('Home');
   });
 });
