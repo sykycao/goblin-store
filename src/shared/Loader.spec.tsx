894bf8e4ae32891bbc8e7d5a42f8383a644ce6ea
Kỳ Cao <sykycao@gmail.com>
2021-10-19 22:29:21 +0700

    test the Loader component

--- src/shared/Loader.spec.tsx
+++ src/shared/Loader.spec.tsx
@@ -1,5 +1,10 @@
-import React from 'react'
+import { render } from '@testing-library/react';
+import React from 'react';
+import { Loader } from './Loader';
 
-describe("Loader", () => {
-  it.todo("renders correctly")
-})
+describe('Loader', () => {
+  it('renders correctly', () => {
+    const { container } = render(<Loader />);
+    expect(container.innerHTML).toMatch('Loading');
+  });
+});
