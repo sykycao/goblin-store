9b0b0330079d34bf176d597ce60db4e514517ae8
Kỳ Cao <sykycao@gmail.com>
2021-10-19 22:24:23 +0700

    test the CartWidget component

--- src/shared/CartWidget/CartWidget.spec.tsx
+++ src/shared/CartWidget/CartWidget.spec.tsx
@@ -1,5 +1,29 @@
-import { CartWidget } from './CartWidget'
+import { fireEvent } from '@testing-library/dom';
+import React from 'react';
+import { CartWidget } from './CartWidget';
 
-describe("CartWidget", () => {
-  it.todo("renders correctly")
-})
+describe('CartWidget', () => {
+  it('shows the amount of products in the cart', () => {
+    const stubCartHook = () => ({
+      products: [
+        {
+          name: 'Product foo',
+          price: 0,
+          image: 'image.png',
+        },
+      ],
+    });
+
+    const { container } = renderWithRouter(() => (
+      <CartWidget useCartHook={stubCartHook} />
+    ));
+
+    expect(container.innerHTML).toMatch('1');
+  });
+
+  it('navigates to cart summary page on click', () => {
+    const { getByRole, history } = renderWithRouter(() => <CartWidget />);
+    fireEvent.click(getByRole('link'));
+    expect(history.location.pathname).toEqual('/cart');
+  });
+});
