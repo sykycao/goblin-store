5b18fc13526eaba30eb32979bb85749d4c6104c7
Kỳ Cao <sykycao@gmail.com>
2021-10-19 23:10:25 +0700

    test the Home component

--- src/Home/Home.spec.tsx
+++ src/Home/Home.spec.tsx
@@ -1,5 +1,71 @@
-import React from 'react'
+import { render } from '@testing-library/react';
+import React from 'react';
+import { Home } from '.';
+import { Category } from '../shared/types';
+import { ProductProps } from './ProductCard';
 
-describe("Home", () => {
-  it.todo("render list of available products") 
-})
+jest.mock('./ProductCard', () => ({
+  ProductCard: ({ datum }: ProductProps) => {
+    const { name, price, image } = datum;
+    return (
+      <div>
+        {name} {price} {image}
+      </div>
+    );
+  },
+}));
+
+describe('Home', () => {
+  describe('while loading', () => {
+    it('renders loader', () => {
+      const mockUseProducts = () => ({
+        categories: [],
+        isLoading: true,
+        error: false,
+      });
+
+      const { container } = render(<Home useProductsHook={mockUseProducts} />);
+
+      expect(container.innerHTML).toMatch('Loading');
+    });
+  });
+
+  describe('with data', () => {
+    const category: Category = {
+      name: 'Category Foo',
+      items: [
+        {
+          name: 'Product foo',
+          price: 55,
+          image: '/test.jpg',
+        },
+      ],
+    };
+    it('renders categories with products', () => {
+      const mockUseProducts = () => ({
+        categories: [category],
+        isLoading: false,
+        error: false,
+      });
+
+      const { container } = render(<Home useProductsHook={mockUseProducts} />);
+
+      expect(container.innerHTML).toMatch('Category Foo');
+      expect(container.innerHTML).toMatch('Product foo 55 /test.jpg');
+    });
+  });
+
+  describe('with error', () => {
+    it('renders error message', () => {
+      const mockUseProducts = () => ({
+        categories: [],
+        isLoading: false,
+        error: true,
+      });
+
+      const { container } = render(<Home useProductsHook={mockUseProducts} />);
+
+      expect(container.innerHTML).toMatch('Error');
+    });
+  });
+});
--- src/Home/ProductCard.tsx
+++ src/Home/ProductCard.tsx
@@ -1,22 +1,20 @@
-import React from "react"
-import { useCart } from "../CartContext"
-import { Product } from "../shared/types"
+import React from 'react';
+import { useCart } from '../CartContext';
+import { Product } from '../shared/types';
 
-interface ProductProps {
+export interface ProductProps {
   datum: Product;
 }
 
 export const ProductCard = ({ datum }: ProductProps) => {
-  const { addToCart, products } = useCart()
+  const { addToCart, products } = useCart();
 
-  const isInCart = !!products?.find((product) => datum.name === product.name)
+  const isInCart = !!products?.find((product) => datum.name === product.name);
 
   return (
-    <div
-      className="nes-container is-rounded item"
-    >
+    <div className="nes-container is-rounded item">
       <img
-        style={{ imageRendering: "pixelated" }}
+        style={{ imageRendering: 'pixelated' }}
         src={datum.image}
         width="64px"
         height="64px"
@@ -27,12 +25,15 @@ export const ProductCard = ({ datum }: ProductProps) => {
       {isInCart ? (
         <button className="nes-btn is-disabled">Added to cart</button>
       ) : (
-        <button className="nes-btn is-primary"
-      onClick={() => {
-        addToCart(datum)
-      }}
-         >Add to cart</button>
+        <button
+          className="nes-btn is-primary"
+          onClick={() => {
+            addToCart(datum);
+          }}
+        >
+          Add to cart
+        </button>
       )}
     </div>
-  )
-}
+  );
+};
